8eb7009602f715352f75105e242477f61e53523c
Casey Hudler <chudler624@gmail.com>
2022-08-17 11:50:53 -0400

    initial commit

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Linq;
+using System.Collections.Generic;
 namespace ArraysAndLists
 {
     class Program
@@ -8,12 +9,43 @@ namespace ArraysAndLists
         {
             //TODO:
 
+            int[] oneToTen= {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            var even = new List<int>();
+            var  odd = new List<int>();
+
+
             // Create an int Array and populate numbers 1-10
 
             /* Create two Lists of type int.
              * Name one List "evens"
              * Name the other List "odds"
              */
+
+            for (int i = 0; i < oneToTen.Length; i++)
+            {
+                if (oneToTen[i] % 2 == 0)
+                {
+                    even.Add(oneToTen[i]);
+                }
+                else
+                {
+                    odd.Add(oneToTen[i]);
+                }
+
+
+            }
+
+            Console.WriteLine("These are even");
+                foreach (var item in even)
+                {
+                    Console.WriteLine(item);
+                }
+            Console.WriteLine("These are odd");
+                foreach (var item in odd)
+                {
+                    Console.WriteLine(item);
+                }
 
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
@@ -27,6 +59,13 @@ namespace ArraysAndLists
              *
              * Try to be creative in your display
              */
+        }
+    }
+
+    internal class list<T>
+    {
+        public list()
+        {
         }
     }
 }
